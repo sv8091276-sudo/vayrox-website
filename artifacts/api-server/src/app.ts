5a2c00dc13ecb97fcf0151ec739e5982d4672b0d
sv8091276-sudo <sv8091276@gmail.com>
2026-07-28 23:59:22 +0530

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -5,26 +5,28 @@ import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
+
 // @ts-ignore
 app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req:any) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res:any) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
       },
     },
-  }),
+  })
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
